53b26e70eb3e461243e6aa75ab67606525b99796
Rafi Barash <rafabarash@gmail.com>
2020-09-12 14:35:20 -0700

    added node package

--- main.go
+++ main.go
@@ -3,11 +3,29 @@ package main
 import (
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/rafibarash/naivechain/block"
+	"github.com/rafibarash/naivechain/node"
+)
+
+var (
+	nodes []*node.Node
 )
 
 func main() {
 	blockchain := block.NewChain()
 	log.Printf("%v", blockchain)
+	initServer()
 	return
+}
+
+func initServer() {
+	r := gin.Default()
+	r.GET("/blocks/:nodeID", func(c *gin.Context) {
+		id := c.Param("nodeID")
+		log.Printf("%s", id)
+
+	})
+
+	log.Panic(r.Run(":8000"))
 }
---
+++ node/node.go
@@ -0,0 +1,7 @@
+package node
+
+import "github.com/rafibarash/naivechain/block"
+
+type Node struct {
+	blockchain []block.Block
+}
